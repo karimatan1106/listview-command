80167aac16636e484005c4af35ba583848282f71
karimata110611 <karimata1106@gmail.com>
2017-05-02 21:32:11 +0900

    ListView Command
    ListViewタップ時にコマンドを実行させる

---
+++ OXamarin/OXamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using System.Collections.Generic;
+
+namespace OXamarin.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private List<string> _items;
+        public List<string> Items
+        {
+            get { return _items; }
+            set { SetProperty(ref _items, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            _items = new List<string>
+            {
+                "コラ・コーラ",
+                "フェンタ",
+                "アクエリオンロゴス",
+                "ポカリスェット",
+                "綾鷲",
+            };
+        }
+    }
+}
---
+++ OXamarin/OXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace OXamarin.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// コードビハインドにItemTappedイベント発火時のコードを書く必要がある
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            entry.Text = e.Item.ToString();
+        }
+    }
+}
